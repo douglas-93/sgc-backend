33173a91a1c42e582b8b19e3b9f55da699bec907
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2022-10-15 12:31:44 -0300

    Put e Delete implementados

--- src/main/java/com/dolts/sgcback/controllers/CemiterioController.java
+++ src/main/java/com/dolts/sgcback/controllers/CemiterioController.java
@@ -2,6 +2,7 @@ package com.dolts.sgcback.controllers;
 
 import com.dolts.sgcback.models.CemiterioModel;
 import com.dolts.sgcback.repositories.CemiterioRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class CemiterioController {
 
     @GetMapping
     public ResponseEntity<List<CemiterioModel>> todosCemiterios() {
-        return ResponseEntity.status(HttpStatus.OK).body(cemiterioRepository.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(cemiterioRepository.findAll(Sort.by(Sort.Direction.ASC, "id")));
     }
 
     @GetMapping(path = "/{id}")
@@ -38,4 +39,27 @@ public class CemiterioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(cemiterioRepository.save(cemiterioModel));
     }
 
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<CemiterioModel> editaCemiterio(@PathVariable int id, @RequestBody CemiterioModel cemiterioModel) {
+        return cemiterioRepository.findById(id).map(
+                data -> {
+                    data.setNome(cemiterioModel.getNome());
+                    data.setEndereco(cemiterioModel.getEndereco());
+                    data.setNumero(cemiterioModel.getNumero());
+                    data.setCidade(cemiterioModel.getCidade());
+                    data.setEstado(cemiterioModel.getEstado());
+                    data.setResponsavel(cemiterioModel.getResponsavel());
+                    CemiterioModel atualizado = cemiterioRepository.save(data);
+                    return ResponseEntity.ok().body(atualizado);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/del/{id}")
+    public ResponseEntity deletaUnidade(@PathVariable int id) {
+        return cemiterioRepository.findById(id)
+                .map(record -> {
+                    cemiterioRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
 }
